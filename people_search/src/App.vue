6a85d8bf3d660a5470ce45a64b9f4289fcf1bec9
Борис Шишкин <shishkinb13@gmail.com>
2019-06-22 17:47:49 +0300

    Создал структуру

---
+++ people_search/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <header>
+            <component :is="'cats'"></component>
+        </header>
+        <router-link to="/">Главная</router-link>
+        <router-link to="/cats">Категории</router-link>
+        <router-link to="/event">Событие</router-link>
+        <router-link to="/event/add">Добавить событие</router-link>
+        <router-view></router-view>
+    </div>
+
+</template>
+
+<script>
+    import Vue from 'vue'
+    import VueRouter from 'vue-router'
+    import index from './components/index2'
+    import cats from './components/cats'
+    import viewEvent from './components/event/index'
+    import addEvent from './components/event/add'
+    Vue.use(VueRouter);
+    const router = new VueRouter({
+        mode: 'history',
+        routes: [
+            {
+                path:'/',
+                meta:{
+                    breadcrumbs: 'Главная'
+                },
+                component:index,
+                name:'index',
+            },
+            {
+                path:'/cats',
+                meta:{
+                    breadcrumbs: 'Категории'
+                },
+                component:cats,
+                name:'cats',
+            },
+            {
+                path:'/event',
+                meta:{
+                    breadcrumbs: 'Событие'
+                },
+                component:viewEvent,
+                name:'/event',
+                children:[
+                    {
+                        path:'add',
+                        component:addEvent,
+                        meta:{
+                            breadcrumbs: 'Добавить событие'
+                        },
+                        name:'addEvent'
+                    }
+                ]
+            }
+        ]
+    });
+
+    export default({
+        name: "App",
+        router,
+        components:{
+            'cats':cats
+        }
+    })
+</script>
+
+<style scoped>
+
+</style>
--- people_search/src/main.js
+++ people_search/src/main.js
@@ -1,41 +1,4 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import App from './components/index2'
+import App from './App'
 
-Vue.use(VueRouter);
-
-Vue.config.productionTip = false;
-const router = new VueRouter([
-  {
-    path:'',
-    meta:{
-      breadcrumbs:'Главная'
-    },
-    name:'index',
-    component:'./components/index2',
-    children:[
-      {
-        path:'cats',
-        meta:{
-          breadcrumbs: 'Категории'
-        },
-        name:'cats',
-
-      },
-      {
-        path:'event',
-        meta:{
-          breadcrumbs: 'Событие'
-        },
-        name:'event'
-      }
-    ]
-  }
-
-]);
-
-
-new Vue({
-  render: h => h(App),
-  router
-}).$mount('#app');
+new Vue({render: h => h(App)}).$mount('#app');
