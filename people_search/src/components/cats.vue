4b4728bc5b191bfad9568d0b296d2913a4e64d8a
Дима <dimas999000pro@mail.ru>
2019-06-22 19:46:47 +0300

    доделал шапку и cats

--- people_search/src/components/cats.vue
+++ people_search/src/components/cats.vue
@@ -3,10 +3,39 @@
         <header>
             <comHeader></comHeader>
         </header>
-        <router-link to="/">Главная</router-link>
-
-        <router-link to="/event">Событие</router-link>
-        <router-link to="/event/add">Добавить событие</router-link>
+        <body>
+            <h1>События</h1>
+            <div class="row">
+                <div class="col-10 tabsSobityi">
+                    <p>Транспортные</p>
+                    <div class="rectangle">> </div>
+                </div>
+                <div class="col-10 tabsSobityi">
+                    <p>Пожары</p>
+                    <div class="rectangle">> </div>
+                </div>
+                <div class="col-10 tabsSobityi">
+                    <p>Обрушение зданий</p>
+                    <div class="rectangle">> </div>
+                </div>
+                <div class="col-10 tabsSobityi">
+                    <p>Гедрогеологические</p>
+                    <div class="rectangle">> </div>
+                </div>
+                <div class="col-10 tabsSobityi">
+                    <p>Наводнения</p>
+                    <div class="rectangle">> </div>
+                </div>
+                <div class="col-10 tabsSobityiRed">
+                    <p>Обрушение зданий</p>
+                    <div class="rectangle">> </div>
+                </div>
+                <div class="col-10 tabsSobityiRed">
+                    <p>Выброс АХОВ</p>
+                    <div class="rectangle">> </div>
+                </div>
+            </div>
+        </body>
     </div>
 </template>
 
@@ -22,5 +51,39 @@
 </script>
 
 <style scoped>
-
+    h1{
+        color: #02248C;
+        text-align: center;
+    }
+    .tabsSobityi{
+        width: 50px;
+        background: rgba(83, 189, 0, 0.3);
+        border-radius: 10px;
+        margin: 0 auto;
+        padding-top: 10px;
+        margin-bottom: 20px;
+    }
+    .tabsSobityiRed{
+        width: 50px;
+        background: rgba(216, 42, 4, 0.3);
+        border-radius: 10px;
+        margin: 0 auto;
+        padding-top: 10px;
+        margin-bottom: 20px;
+    }
+    .tabsSobityiRed p{
+        font-size: 19px;
+        font-style: normal;
+        font-weight: normal;
+    }
+    .tabsSobityi p{
+        font-size: 19px;
+        font-style: normal;
+        font-weight: normal;
+    }
+    .rectangle{
+        font-size: 30px;
+        margin-top: -50px;
+        float: right;
+    }
 </style>
