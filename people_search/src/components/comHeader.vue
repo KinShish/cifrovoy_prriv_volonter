ec6218a3864410eb9252a0ced5037e8605b6ce81
Андрей Даценко <adatsenko99@mail.ru>
2019-06-23 00:03:12 +0300

    меню

--- people_search/src/components/comHeader.vue
+++ people_search/src/components/comHeader.vue
@@ -6,28 +6,29 @@
             </div>
             <div class="col-2">
                 <template>
-                    <Slide right width="250">
-                        <router-link to="/cats">
-                            <span>Государственные события</span>
+                    <Slide right noOverlay>
+                        <router-link class="dropA" to="/cats">
+                            <span class="drop">Государственные события</span>
                         </router-link>
-                        <router-link to="/cats">
-                            <span>Муниципальные события</span>
+                        <router-link class="dropA" to="/cats">
+                            <span class="drop">Муниципальные события</span>
                         </router-link>
-                        <router-link to="/cats">
-                            <span>Гражданские события</span>
+                        <router-link class="dropA" to="/cats">
+                            <span class="drop">Гражданские события</span>
                         </router-link>
-                        <router-link to="/cats">
-                            <span>События в моей зоне</span>
+                        <router-link class="dropA" to="/cats">
+                            <span class="drop">События в моей зоне</span>
                         </router-link>
                     </Slide>
                 </template>
             </div>
-            <div class="col-12">
-                <ul class="buttonregistration">
-                    <li><a href="#">Регистрация /</a></li>
-                    <li><a href="#">Вход</a></li>
-                </ul>
-            </div>
+                <div class="col-12">
+                    <p class="back"><span>&#8592;</span>Назад</p>
+                    <ul class="buttonregistration">
+                        <li><a href="#">Регистрация /</a></li>
+                        <li><a href="#">Вход</a></li>
+                    </ul>
+                </div>
         </div>
     </div>
 </template>
@@ -52,8 +53,9 @@
         margin-top: 10px;
     }
     .buttonregistration{
-        margin-top: 70px;
+        margin-top: -15px;
         text-align: right;
+        float: right;
     }
     .buttonregistration a{
         background: none;
@@ -76,5 +78,38 @@
         border: 1px solid black;
         float:right;
         margin-top: 5px;
+    }
+    .drop{
+        font-size: 15px;
+    }
+    .cross-style{
+        left: 10px;
+    }
+    .bm-burger-button {
+        position: fixed;
+        width: 36px;
+        height: 20px;
+        left: 36px;
+        top: 36px;
+        cursor: pointer;
+    }
+    .bm-menu {
+        height: 100%; /* 100% Full-height */
+        width: 0; /* 0 width - change this with JavaScript */
+        position: fixed; /* Stay in place */
+        z-index: 1000; /* Stay on top */
+        top: 0;
+        left: 0;
+        background-color: rgb(63, 63, 65); /* Black*/
+        overflow-x: hidden; /* Disable horizontal scroll */
+        padding-top: 60px; /* Place content 60px from the top */
+        transition: 0.5s; /*0.5 second transition effect to slide in the sidenav*/
+    }
+    .dropA:hover{
+        text-decoration: none;
+        background-color: #303032;
+    }
+    .back{
+        display: inline-block;
     }
 </style>
